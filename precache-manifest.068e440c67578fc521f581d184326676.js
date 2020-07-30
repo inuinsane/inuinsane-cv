bcb771f65f504c704f285057820a46f80edd4efc
Abdullah Ibnu Hasan <38773546+inuinsane@users.noreply.github.com>
2020-07-30 14:45:21 +0800

    initial commit

---
+++ precache-manifest.068e440c67578fc521f581d184326676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b30305704327a953d1b790f8fd69afd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "85a2f8acc3e6c9b79f83",
+    "url": "./static/css/main.a2ebf65f.chunk.css"
+  },
+  {
+    "revision": "64db31915a6ddc6a9e75",
+    "url": "./static/js/2.3bfb19fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.3bfb19fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a2f8acc3e6c9b79f83",
+    "url": "./static/js/main.8c41960d.chunk.js"
+  },
+  {
+    "revision": "d1e4e538097bff66056d",
+    "url": "./static/js/runtime-main.275cdbec.js"
+  },
+  {
+    "revision": "dc32104d1889bdfd5be0ed1131dc58fd",
+    "url": "./static/media/avatar.dc32104d.png"
+  }
+]);
